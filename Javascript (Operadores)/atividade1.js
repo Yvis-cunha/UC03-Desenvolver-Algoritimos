c790fb86214793173acc586b1616043786b352b1
Yvis Igor <ivisigor3@gmail.com>
2025-08-19 20:42:51 -0300

    Ataulização da atividade 1

--- Javascript (Operadores)/atividade1.js
+++ Javascript (Operadores)/atividade1.js
@@ -6,16 +6,3 @@ let idadeanos, idadedias//variaveis
 idadeanos = Number(prompt('Digite sua Idade em anos: '))
 idadedias = idadeanos * 365
 console.log("Sua idade em dias é: ",idadedias )
-
-
-
-
-
-
-
-//const prompt = require("prompt-sync")();
-
-/*let nome = 'Yvis'
-const idade = 31
-console.log("Meu nome é:",nome,"e minha idade é: ",idade )
-*/
