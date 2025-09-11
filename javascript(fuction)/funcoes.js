90388b8c8afc94b37c4589307fd30ea3cb12d3cb
Yvis-Cunha <ivisigor3@gmail.com>
2025-09-11 02:10:50 -0300

    Novas atividades realizadas

--- javascript(fuction)/funcoes.js
+++ javascript(fuction)/funcoes.js
@@ -8,3 +8,14 @@ export function deposito(saldo, valor){
     saldo = (saldo + valor)
     return saldo
 }
+//Função média aritimetrica
+export function media (n1,n2,n3,resultado){
+    resultado = (n1+n2+n3)/3 //pergunta por que preciso declarar a variavel aqui sendo q as outras atividades não precisou
+    return resultado
+}
+
+//Função converte fahrenheit para celcius
+export function celciusPfahen (celcius){
+    let valorfinal = (celcius*1.8)+32
+    return valorfinal
+}
