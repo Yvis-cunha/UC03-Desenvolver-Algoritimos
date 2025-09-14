8a2a9da0c02a91aced892bedd044c6c718347016
Yvis-Cunha <ivisigor3@gmail.com>
2025-09-14 18:53:26 -0300

    Novas atividades

---
+++ javascript(Métodos para array callback)/atividade1.js
@@ -0,0 +1,9 @@
+import PromptSync from "prompt-sync" // Importe do prompt
+const prompt = PromptSync() // variavel do prompt
+
+let nomes = []
+let hortfruti=['Frutas','Verduras','Legumes']
+let presco = []
+
+
+console.log(hortfruti)
