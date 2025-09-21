72a2bd27fb4ff929e84eccfc0db6d8ae2f3f94a5
Yvis-Cunha <ivisigor3@gmail.com>
2025-09-21 18:40:58 -0300

    Atividade 3 concluida

--- javascript(orientada objeto)/atividade3.js
+++ javascript(orientada objeto)/atividade3.js
@@ -2,6 +2,7 @@ import PromptSync from "prompt-sync" // Importe do prompt
 const prompt = PromptSync() // variavel do prompt
 
 
+
 class Produto{
     nome
     preco
@@ -10,8 +11,34 @@ class Produto{
     disponivel
 
     exibirinformacoes(){
-        
+        console.log(`Nome: ${this.nome}\nPresço: R$${this.preco}\nQtdeEstoque: ${this.quantidadeestoque}\nCategoria:${this.categoria}\nDisponibilidade: ${this.disponivel}\n`)
     }
-
+    mostrarquantidade(){
+        console.log(`Quantidade em estoque disponivel: ${this.quantidadeestoque} `)
+    }
+    adicionarEstoque(){
+        let maisprodutoNumber = Number (prompt("Quanto deseja adicionar ao estoque? "))
+        quantidadeestoque = quantidadeestoque + maisprodutoNumber
+    }
+    removerEstoque(){
+        let menosproduto = Number (prompt("Quanto deseja adicionar ao estoque? "))
+        quantidadeestoque = quantidadeestoque - menosproduto
+    }
+    definirPresco(){
+        let preco = Number (prompt(`Digite o presço do produto:`))
+    }
+    verificarDisponibilidade(){
+        console.log(`Disponibilidade: ${this.disponivel} `)
+    }
 }
+let mostrar = new Produto()
 
+mostrar.nome = prompt("Digite o nome do produto: ")
+mostrar.preco = Number (prompt("Digite o presço: "))
+mostrar.quantidadeestoque = Number (prompt("Digite a quantidade de estoque: "))
+mostrar.categoria = prompt("Digite a categoria: ")
+mostrar.disponivel = prompt("Digite se está disponivel a venda: ")
+
+mostrar.exibirinformacoes()
+
+mostrar.definirPresco()
