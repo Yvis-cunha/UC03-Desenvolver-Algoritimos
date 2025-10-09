e1223b1c63a434423ed7e3208972f469e4bcc218
Yvis-Cunha <ivisigor3@gmail.com>
2025-10-09 01:47:26 -0300

    urna eletronica

--- projetinhos simples/urna.js
+++ projetinhos simples/urna.js
@@ -1,14 +1,53 @@
 import PromptSync from "prompt-sync" // Importe do prompt
 const prompt = PromptSync() // variavel do prompt
 
-let arraycanidatos = []
-let arrayvotos = []
+let canidatos = []
 
 console.log("====URNA ELETRONICA====")
-let qtd = prompt("Digite a quantidade de canidatos: ")
 
-for(let i = 0; i < qtd; i = i + 1){
-    arraycanidatos.push(prompt("Digite o nome do canidato: "))
+for(let l = 0; l < 3; l= l + 1){
+    canidatos [l]=[]
+        for(let c = 0; c < 2; c++){
+            if(c === 0){
+            canidatos[l][c] = (prompt("Digite o nome do canidato: "))
+            }else{
+                canidatos[l][c] = 0
+            }
+        }
 }
-console.log(arraycanidatos)
+let voto
+do{
+    voto = prompt(`
+    ===Agora vamos realizar a votação===
+    1- ${canidatos[0][0]}
+    2- ${canidatos[1][0]}
+    3- ${canidatos[2][0]}
+    0- Finaliza votação
+    `)
+
+switch(voto){
+    case '1':
+       canidatos[0][1] = canidatos[0][1] + 1
+    break
+    case '2':
+        canidatos[1][1] = canidatos[1][1] + 1
+    break
+    case '3':
+        canidatos[2][1] = canidatos[2][1] + 1     
+
+    }
+}while(voto != 0)
+
+    if(canidatos[0][1] > canidatos[1][1] && canidatos[0][1] > canidatos[2][1]){
+        console.log("O vencedor é: ", canidatos[0][0])
+    }else if(canidatos[1][1] > canidatos[0][1] && canidatos[1][1] > canidatos[2][1]){
+        console.log("O vencedor é ", canidatos[1][0])
+    }else if(canidatos[2][1] > canidatos[0][1] && canidatos[2][1] > canidatos[1][1]){
+        console.log("O vencedor é: ", canidatos[2][0])
+    }
+
+
+console.log(`Canidato: ${canidatos[0][0]} Votos: ${canidatos[0][1]}`)
+console.log(`Canidato: ${canidatos[1][0]} Votos: ${canidatos[1][1]}`)
+console.log(`Canidato: ${canidatos[2][0]} Votos: ${canidatos[2][1]}`)
 
